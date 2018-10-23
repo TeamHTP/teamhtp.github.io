82720e7e18567156e5b2253e970274ee3502aecf
Tianyi Ma <tn.ma@outlook.com>
2018-10-23 09:54:07 -0500

    Code

---
+++ index.js
@@ -0,0 +1,31 @@
+var htpApp = new Vue({
+  el: '#app',
+  data: {
+    ghUsers: []
+  }
+});
+
+var teamLogins = [
+  'cata85',
+  'dominictassio',
+  'tma02'
+];
+
+function getUser(login, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.github.com/users/${login}`, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      callback(xhr.status, xhr.responseText);
+    }
+  }
+  xhr.send();
+}
+
+teamLogins.forEach((login) => {
+  getUser(login, (status, responseText) => {
+    if (status == 200) {
+      htpApp.ghUsers.push(JSON.parse(responseText));
+    }
+  });
+});
